e85a9122f5c69f2039febd263efebdf5d8fc15a8
Hema <hemasahu019@gmail.com>
2022-07-29 17:58:42 +0530

    Added UC1 Code to find the Max integer value

--- Program.cs
+++ Program.cs
@@ -1,12 +1,18 @@
 ﻿namespace FindMaxProblemUsingGenerics
 { /// <summary>
-  /// Welcome Page
+  /// UC1 : Given 3 Integers find the maximum
   /// </summary>
     internal class Program
-    {    
+    {
+        //An entrypoint of main program
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Find Maximum Problem using Generics"); //Welcome Message
+            //Integers variables
+            int val1 = 85, val2 = 78, val3 = 95;
+
+           //Displaying max value for Integers
+            Console.WriteLine("\nThe Maximum value of Integer out of {0}, {1} and {2} is {3}", val1, val2, val3, FindTheMax.FindTheMaxValue(val1, val2, val3));
+            
         }
     }
 }
