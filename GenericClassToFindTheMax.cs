98abbd67b8b08c90b8afc4988a195868fd41ca52
Hema <hemasahu019@gmail.com>
2022-07-29 18:28:12 +0530

    Refactor code by using generic method

---
+++ GenericClassToFindTheMax.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaxProblemUsingGenerics
+{
+    public class GenericClassToFindTheMax<T> where T : IComparable
+    {    
+        //Generic Method to find Max Value
+        public static T FindTheMaxValue(T val1, T val2, T val3)
+        {
+            if ((val1.CompareTo(val2) >= 0) && (val1.CompareTo(val3) >= 0))
+                return val1;
+            else if ((val2.CompareTo(val1) >= 0) && (val2.CompareTo(val3) >= 0))
+                return val2;
+            else if ((val3.CompareTo(val1) >= 0) && (val3.CompareTo(val2) >= 0))
+                return val3;
+            else
+                return default;  //return based on the data type
+        }
+   
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace FindMaxProblemUsingGenerics
 { /// <summary>
-  /// UC3 : Given 3 strings find the maximum
+  /// Refactor1 : Refactor all the 3 to One Generic Method and find the maximum
   /// </summary>
     internal class Program
     {
@@ -17,11 +17,11 @@
             string val7 = "Apple", val8 = "Peach", val9 = "Banana";
 
             //Displaying max value for Integers
-            Console.WriteLine("\nThe Maximum value of Integer out of {0}, {1} and {2} is {3}", val1, val2, val3, FindTheMax.FindTheMaxValue(val1, val2, val3));
+            Console.WriteLine("\nThe Maximum value of Integer out of {0}, {1} and {2} is {3}", val1, val2, val3, GenericClassToFindTheMax<int>.FindTheMaxValue(val1, val2, val3));
             //Displaying max value for Floats
-            Console.WriteLine("\nThe Maximum value of Float out of {0}, {1} and {2} is {3}", val4, val5, val6, FindTheMax.FindTheMaxValue(val4, val5, val6));
+            Console.WriteLine("\nThe Maximum value of Float out of {0}, {1} and {2} is {3}", val4, val5, val6, GenericClassToFindTheMax<float>.FindTheMaxValue(val4, val5, val6));
             //Displaying max value for Strings
-            Console.WriteLine("\nThe Maximum value of string out of {0}, {1} and {2} is {3}", val7, val8, val9, FindTheMax.FindTheMaxValue(val7, val8, val9));
+            Console.WriteLine("\nThe Maximum value of string out of {0}, {1} and {2} is {3}", val7, val8, val9, GenericClassToFindTheMax<string>.FindTheMaxValue(val7, val8, val9));
 
         }
     }
