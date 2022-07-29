ce6812c2c101418fce125a04f0b4404303d7257b
Hema <hemasahu019@gmail.com>
2022-07-29 18:05:17 +0530

    Added UC3 Code to find the Max string value

--- FindTheMax.cs
+++ FindTheMax.cs
@@ -35,5 +35,17 @@ namespace FindMaxProblemUsingGenerics
                 return 0;
         }
 
+        //Method to find Max Value of string
+        public static string FindTheMaxValue(string val1, string val2, string val3)
+        {
+            if ((val1.CompareTo(val2) >= 0) && (val1.CompareTo(val3) >= 0))
+                return val1;
+            else if ((val2.CompareTo(val1) >= 0) && (val2.CompareTo(val3) >= 0))
+                return val2;
+            else if ((val3.CompareTo(val1) >= 0) && (val3.CompareTo(val2) >= 0))
+                return val3;
+            else
+                return "";
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace FindMaxProblemUsingGenerics
 { /// <summary>
-  /// UC2 : Given 3 float find the maximum
+  /// UC3 : Given 3 strings find the maximum
   /// </summary>
     internal class Program
     {
@@ -13,11 +13,15 @@
             //Float variables
             float val4 = 85.35f, val5 = 78.85f, val6 = 95.78f;
 
+            //String variables
+            string val7 = "Apple", val8 = "Peach", val9 = "Banana";
+
             //Displaying max value for Integers
             Console.WriteLine("\nThe Maximum value of Integer out of {0}, {1} and {2} is {3}", val1, val2, val3, FindTheMax.FindTheMaxValue(val1, val2, val3));
             //Displaying max value for Floats
             Console.WriteLine("\nThe Maximum value of Float out of {0}, {1} and {2} is {3}", val4, val5, val6, FindTheMax.FindTheMaxValue(val4, val5, val6));
-            
+            //Displaying max value for Strings
+            Console.WriteLine("\nThe Maximum value of string out of {0}, {1} and {2} is {3}", val7, val8, val9, FindTheMax.FindTheMaxValue(val7, val8, val9));
 
         }
     }
