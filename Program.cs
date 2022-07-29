ef1b06ce59d524f32574fb9a41fc8fdac4e74504
Hema <hemasahu019@gmail.com>
2022-07-29 17:53:50 +0530

    Added Welcome Page code to main

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace FindMaxProblemUsingGenerics
+{ /// <summary>
+  /// Welcome Page
+  /// </summary>
+    internal class Program
+    {    
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Find Maximum Problem using Generics"); //Welcome Message
+        }
+    }
+}
